437f302318c100e8648010b62c07f50999ddc975
k0tokay <astl6446229489@gmail.com>
2024-11-12 21:46:06 +0900

    scssの警告の対処

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/kono-dictionary-editor/"
+  base: "/kono-dictionary-editor/",
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern-compiler",
+      },
+    }
+  }
 })
